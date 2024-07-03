dea081412769f0aa49c82557e49c2eddeb52538f
Baylospawn <microsoftbaylo@laposte.net>
2024-07-03 20:42:36 +0200

    Rajout de la route galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,8 @@ import Route from "./Route.js";
 
 //Définir les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "Galerie", "pages/galerie.html", "/js/galerie.js"),
+];
 
 export const websiteName = "Arcadia";
