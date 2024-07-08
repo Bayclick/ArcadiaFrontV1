b9d4acccc38a92cd53420eaf638f87a2f6d60ac6
Baylospawn <microsoftbaylo@laposte.net>
2024-07-08 19:48:52 +0200

    Création route Services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir les routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "pages/galerie.html", "/js/galerie.js"),
+    new Route("/services", "Services", "/pages/services.html"),
 ];
 
 export const websiteName = "Arcadia";
